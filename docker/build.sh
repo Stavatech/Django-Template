a1cb06a5b57d5b2276e7134276f348096f05bab9
davidbrownza <davidbrownza@outlook.com>
2019-08-30 17:52:42 +0200

    Added missing space

--- docker/build.sh
+++ docker/build.sh
@@ -5,5 +5,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 if [ "$STAGE" = "production" ] ; then
     docker build -t {{ project_name }}:latest . -f ${DIR}/Dockerfile.prod
 else
-    docker build -t{{ project_name }}:dev . -f ${DIR}/Dockerfile.dev
+    docker build -t {{ project_name }}:dev . -f ${DIR}/Dockerfile.dev
 fi
